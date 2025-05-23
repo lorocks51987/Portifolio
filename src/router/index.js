ee421372069ffbc5cf9083b5eb8442688691b93c
Luiz Henrique <lorocks57321@gmail.com>
2025-05-23 16:20:06 -0300

    Ajuste no router para GitHub Pages

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import ProjectsView from '../views/ProjectsView.vue'
 import ContactView from '../views/ContactView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/Portifolio/'),
   routes: [
     {
       path: '/',
